82103d121d99561b631eeca8d66c04cc60ae9d33
foaf20@bath.ac.uk <foaf20@bath.ac.uk>
2025-06-09 21:16:13 +0000

    changes

--- AddFlight.py
+++ AddFlight.py
@@ -70,6 +70,57 @@ class AddFlightInfo:
     def __str__(self):
         return str(
         self.flightID
-        ) + "\n" + self.departureAirport + "\n" + self.destinationAirport + "\n" + str(
+        ) + "\n" + self.flightNumber + "\n" + self.departureAirport + "\n" + self.destinationAirport + "\n" + str(
       self.flightStatus)
- 
+    
+    def insert_data(self):
+        try:
+        #self.get_connection()
+            conn2=sqlite3.connect('DBFlight.db')
+            mycur3 = conn2.cursor()
+
+            flight = AddFlightInfo()
+            flight.set_flight_id(mycur3.execute("SELECT COUNT(*) FROM {Flight}"))
+            flight.set_flight_number(int(input("Enter FlightNumber: ")))
+            flight.set_departure_airport(int(input("Enter Departure Airport: ")))
+            flight.set_destination_airport(int(input("Enter Destination Airport: ")))
+            flight.set_flight_status(int(input("Enter Flight Status: ")))
+
+            self.mycur3.execute("INSERT INTO Flight", tuple(str(flight).split("\n")))
+
+            self.conn2.commit()
+            print("Inserted data successfully")
+        except Exception as e:
+            print(e)
+        finally:
+            self.conn.close()
+while True:
+  print("\n Menu:")
+  print("**********")
+  print(" 1. Add a New Flight")
+  #print(" 2. Insert data into FlightInfo")
+  #print(" 3. Select all data from FlightInfo")
+  #print(" 4. Search a flight")
+  #print(" 5. Update data some records")
+  #print(" 6. Delete data some records")
+  print(" 7. Exit\n")
+
+  __choose_menu = int(input("Enter your choice: "))
+  #db_ops = DBOperations()
+  if __choose_menu == 1:
+      AddFlightInfo.insert_data()
+    #db_ops.create_table()
+  #elif __choose_menu == 2:
+   # db_ops.insert_data()
+  #elif __choose_menu == 3:
+    #db_ops.select_all()
+  #elif __choose_menu == 4:
+    #db_ops.search_data()
+  #elif __choose_menu == 5:
+    #db_ops.update_data()
+  #elif __choose_menu == 6:
+    #db_ops.delete_data()
+  elif __choose_menu == 7:
+    exit(0)
+  else:
+    print("Invalid Choice")
