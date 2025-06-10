ae7581df18bdee9971b71f53f2f5c978ba887874
foaf20@bath.ac.uk <foaf20@bath.ac.uk>
2025-06-10 14:32:48 +0000

    still writing

---
+++ ViewFlight.py
@@ -0,0 +1,9 @@
+# this code allows user to view flights by different criterias
+
+class ViewFlight:
+    def __init__(self):
+        self.flightID = 0
+        self.flightStatus = ''
+        self.pilotID = 0
+
+    def searchFlight(self):
